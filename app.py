f9b265aa7119822a154bac63f884de924e56b252
Ubuntu <ubuntu@ip-172-31-34-169.ap-south-1.compute.internal>
2026-07-10 11:21:48 +0000

    Add health endpoint

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1>Hello from Shahriar's CI/CD Pipeline! v2</h1>"
-
+@app.route('/health'
+def health():
+	return "OK", 200
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
