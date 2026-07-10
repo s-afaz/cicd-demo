86b12f395c6d5ceb7758d955d78add7882a41031
Ubuntu <ubuntu@ip-172-31-34-169.ap-south-1.compute.internal>
2026-07-10 11:11:56 +0000

    Update to v2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello from Shahriar's CI/CD Pipeline! v1</h1>"
+    return "<h1>Hello from Shahriar's CI/CD Pipeline! v2</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
